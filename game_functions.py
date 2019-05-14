b1b7eb0d8c5d268145288da494bab2137b850e56
Maria Mileeva <mileeva.mary@gmail.com>
2019-05-14 20:52:03 +0300

    Refactoring (keydown and keyup events)

--- game_functions.py
+++ game_functions.py
@@ -2,24 +2,34 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """ Respond to keypresses (when player press on the keyboard) """
+
+    if event.key == pygame.K_RIGHT:  # When player press "right" arrow key
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:  # When player press "left" arrow key
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """ Respond to key releases """
+
+    if event.key == pygame.K_RIGHT:  # When player press "right" arrow key
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:  # When player press "left" arrow key
+        ship.moving_left = False
+
+
 def check_events(ship):
     """ Respond to keypress and mouse events """
 
     for event in pygame.event.get():  # Access to the events (player's actions)
         if event.type == pygame.QUIT:  # When player clicks window's close button
             sys.exit()  # Exit the game
-
-        elif event.type == pygame.KEYDOWN:  # When player press on the keyboard
-            if event.key == pygame.K_RIGHT:  # When player press "right" arrow key
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:  # When player press "left" arrow key
-                ship.moving_left = True
-
-        elif event.type == pygame.KEYUP:  # ???
-            if event.key == pygame.K_RIGHT:  # When player press "right" arrow key
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:  # When player press "left" arrow key
-                ship.moving_left = False
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
